cd48beba3d1f6ef1231ead7650b42a8a2285b0f0
PeiSheng:RE <peisheng1@hotmail.com>
2017-11-24 14:36:18 +0800

    Added items from Lab 06
    Added Spatial Partitioning, Grids, Grenade, and Grenade Throw

---
+++ Base/Source/WeaponInfo/GrenadeThrow.cpp
@@ -0,0 +1,52 @@
+#include "GrenadeThrow.h"
+#include "../Projectile/Grenade.h"
+CGrenadeThrow::CGrenadeThrow()
+{
+}
+CGrenadeThrow::~CGrenadeThrow()
+{
+}// Initialise this instance to default values
+void CGrenadeThrow::Init(void)
+{
+	// Call the parent's Init method
+	CWeaponInfo::Init();
+	firingspeed = 20.f;
+	// The number of ammunition in a magazine for this weapon
+	magRounds = 1;
+	// The maximum number of ammunition for this magazine for this weapon
+	maxMagRounds = 1;
+	// The current total number of rounds currently carried by this player
+	totalRounds = 2;
+	// The max total number of rounds currently carried by this player
+	maxTotalRounds = 2;
+	// The time between shots
+	timeBetweenShots = 1.0;
+	// The elapsed time (between shots)
+	elapsedTime = 0.0;
+	// Boolean flag to indicate if weapon can fire now
+	bFire = true;
+}
+// Discharge this weapon
+void CGrenadeThrow::Discharge(const std::string& _meshName, Vector3 position, Vector3 target, float bulletSpeed, CPlayerInfo* _source)
+{
+	if (bFire)
+	{
+		// If there is still ammo in the magazine, then fire
+		if (magRounds > 0)
+		{
+			// Create a projectile with a cube mesh. Its position and direction is same as the player.
+				// It will last for 3.0 seconds and travel at 500 units per second
+				CProjectile* aProjectile = Create::Grenade(_meshName,
+					position,
+					(target - position).Normalized(),
+					7.0f,
+					bulletSpeed,
+					_source);
+			aProjectile->SetCollider(true);
+
+			aProjectile->SetAABB(Vector3(0.5f, 0.5f, 0.5f), Vector3(-0.5f, -0.5f, -0.5f));
+			bFire = false;
+			magRounds--;
+		}
+	}
+}
---
+++ Base/Source/WeaponInfo/GrenadeThrow.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "WeaponInfo.h"
+
+class CGrenadeThrow : public CWeaponInfo
+{
+public:
+	CGrenadeThrow();
+	virtual ~CGrenadeThrow();
+	// Initialise this instance to default values
+	void Init(void);
+	// Discharge this weapon
+	void CGrenadeThrow::Discharge(const std::string& _meshName, Vector3 position, Vector3 target, float bulletSpeed, CPlayerInfo* _source);
+};
